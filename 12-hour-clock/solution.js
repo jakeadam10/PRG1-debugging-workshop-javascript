7104319ae7fe9bca543b768d34ae6ebb9073852c
MitchLloyd <mitchell@ada.ac.uk>
2024-08-22 16:25:58 +0000

    capitalise suffix

--- 12-hour-clock/solution.js
+++ 12-hour-clock/solution.js
@@ -2,9 +2,9 @@ function convertTo12Hours(time) {
     const hours = time.slice(0,2);
     const minutes = time.slice(-2);
     if (hours > 12) {
-        return `${hours - 12}:${minutes} pm`
+        return `${hours - 12}:${minutes} PM`
     } else {
-        return `${hours}:${minutes} am` 
+        return `${hours}:${minutes} AM` 
     }
 }
 
