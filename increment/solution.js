c9f8a460b38c0f70b02eee2e20b4b860b0e707b0
MitchLloyd <mitchell@ada.ac.uk>
2024-08-22 13:46:16 +0000

    remove call and add call to find century

--- increment/solution.js
+++ increment/solution.js
@@ -6,4 +6,7 @@ function increment(n) {
     return n++;
 }
 
-console.log(increment(100));
+// Remember to check the expected behaviour of this program first
+// Then call the function to check how it is actually working...
+
+// Write some commments down below with your findings...
