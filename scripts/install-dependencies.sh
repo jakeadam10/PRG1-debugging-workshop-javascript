f3bba7fa0ea62f62d7d1614cd699c6db67d55ebc
MitchLloyd <mitchell@ada.ac.uk>
2024-08-22 14:32:38 +0000

    install bun in zsh profile

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,2 +1,3 @@
-curl -fsSL https://bun.sh/install | bash
 npm install
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" && chsh -s $(which zsh)
+curl -fsSL https://bun.sh/install | bash && echo export BUN_INSTALL="$HOME/.bun" >> "$HOME/.zshrc" && echo export PATH="$BUN_INSTALL/bin:$PATH" >> "$HOME/.zshrc"
